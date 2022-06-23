b273c654c166e0a9f700c86342b76ae110328c5b
sobadfish <3098143605@qq.com>
2022-06-24 00:13:19 +0800

    2022/6/23 v1.4.7 更新

--- src/main/java/org/sobadfish/bedwar/item/ItemInfo.java
+++ src/main/java/org/sobadfish/bedwar/item/ItemInfo.java
@@ -73,25 +73,30 @@ public class ItemInfo {
     }
 
     public static EntityItem getEntityItem(Item item,Position position){
-        CompoundTag itemTag = NBTIO.putItemHelper(item);
-        itemTag.setName("Item");
-        Position position1 = position.add(0.5,0.5,0.5);
-        EntityItem itemEntity = new EntityItem(position1.getLevel().getChunk((int)position1.getX() >> 4, (int)position1.getZ() >> 4, true),
-                new CompoundTag().putList(new ListTag<>("Pos")
-                        .add(new DoubleTag("", position1.getX()))
-                        .add(new DoubleTag("", position1.getY()))
-                        .add(new DoubleTag("", position1.getZ())))
-                        .putShort("Health", 5)
-                        .putCompound("Item", itemTag)
-                        .putList((new ListTag<>("Motion"))
-                                .add(new DoubleTag("", 0))
-                                .add(new DoubleTag("", 0))
-                                .add(new DoubleTag("",0)))
-                        .putList((new ListTag<>("Rotation"))
-                                .add(new FloatTag("", ThreadLocalRandom.current().nextFloat() * 360.0F))
-                                .add(new FloatTag("", 0.0F)))
-                        .putShort("PickupDelay", 10));
-        return itemEntity;
+        try{
+            CompoundTag itemTag = NBTIO.putItemHelper(item);
+            itemTag.setName("Item");
+            Position position1 = position.add(0.5,0.5,0.5);
+            EntityItem itemEntity = new EntityItem(position1.getLevel().getChunk((int)position1.getX() >> 4, (int)position1.getZ() >> 4, true),
+                    new CompoundTag().putList(new ListTag<>("Pos")
+                            .add(new DoubleTag("", position1.getX()))
+                            .add(new DoubleTag("", position1.getY()))
+                            .add(new DoubleTag("", position1.getZ())))
+                            .putShort("Health", 5)
+                            .putCompound("Item", itemTag)
+                            .putList((new ListTag<>("Motion"))
+                                    .add(new DoubleTag("", 0))
+                                    .add(new DoubleTag("", 0))
+                                    .add(new DoubleTag("",0)))
+                            .putList((new ListTag<>("Rotation"))
+                                    .add(new FloatTag("", ThreadLocalRandom.current().nextFloat() * 360.0F))
+                                    .add(new FloatTag("", 0.0F)))
+                            .putShort("PickupDelay", 10));
+            return itemEntity;
+        }catch (Exception e){
+            return null;
+        }
+
 
     }
 
@@ -102,23 +107,25 @@ public class ItemInfo {
     public void toUpdate(){
         if(resetTick > 0 && tick == resetTick || tick == itemInfoConfig.getSpawnTick()){
             tick = 0;
-            itemInfoConfig.getPositions().forEach(position ->{
+            itemInfoConfig.getPositions().forEach(position -> {
 
                 ArrayList<EntityItem> entityItems = new ArrayList<>();
-                for(Entity entity : position.getLevel().getChunkEntities(position.getChunkX(),position.getChunkZ()).values()){
-                    if(entity instanceof EntityItem){
-                        if(((EntityItem) entity).getItem().equals(getItemInfoConfig().getMoneyItemInfoConfig().getItem())){
+                for (Entity entity : position.getLevel().getChunkEntities(position.getChunkX(), position.getChunkZ()).values()) {
+                    if (entity instanceof EntityItem) {
+                        if (((EntityItem) entity).getItem().equals(getItemInfoConfig().getMoneyItemInfoConfig().getItem())) {
                             entityItems.add((EntityItem) entity);
                         }
                     }
                 }
-                if(entityItems.size() > 20){
+                if (entityItems.size() > 20) {
                     return;
                 }
                 Item item = getItemInfoConfig().getMoneyItemInfoConfig().getItem();
-                getEntityItem(item,position).spawnToAll();
-
-            } );
+                EntityItem entityItem = getEntityItem(item, position);
+                if (entityItem != null) {
+                    entityItem.spawnToAll();
+                }
+            });
         }else{
             tick++;
         }
