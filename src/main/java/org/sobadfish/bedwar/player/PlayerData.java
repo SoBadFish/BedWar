21eaafdc421cf03a4dad67d30214f8b3fa8b6b40
sobadfish <3098143605@qq.com>
2022-09-04 19:21:17 +0800

    v1.8.0 修复一些BUG

--- src/main/java/org/sobadfish/bedwar/player/PlayerData.java
+++ src/main/java/org/sobadfish/bedwar/player/PlayerData.java
@@ -155,8 +155,12 @@ public class PlayerData {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             RoomData roomData = (RoomData) o;
             return Objects.equals(roomName, roomData.roomName);
         }
