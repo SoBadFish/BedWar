c2849f3602920814698d0359449de63ef3bda26a
Sobadfish <3098143605>
2022-12-07 14:29:42 +0800

    v1.8.6 优化房间释放

--- src/main/java/org/sobadfish/bedwar/tools/Utils.java
+++ src/main/java/org/sobadfish/bedwar/tools/Utils.java
@@ -315,34 +315,6 @@ public class Utils {
         destFile.close();
     }
 
-
-
-//
-//    private static void copyFile(File sourceFile,File targetFile)
-//            throws IOException{
-//        // 新建文件输入流并对它进行缓冲
-////        FileInputStream input = new FileInputStream(sourceFile);
-////        BufferedInputStream inBuff=new BufferedInputStream(input);
-////
-////        // 新建文件输出流并对它进行缓冲
-////        FileOutputStream output = new FileOutputStream(targetFile);
-////        BufferedOutputStream outBuff=new BufferedOutputStream(output);
-////
-////        // 缓冲数组
-////        byte[] b = new byte[1024 * 5];
-////        int len;
-////        while ((len =inBuff.read(b)) != -1) {
-////            outBuff.write(b, 0, len);
-////        }
-////        // 刷新此缓冲的输出流
-////        outBuff.flush();
-////
-////        //关闭流
-////        inBuff.close();
-////        outBuff.close();
-////        output.close();
-////        input.close();
-//    }
     /**复制文件夹   */
     private static void copyDirectiory(String sourceDir, String targetDir)
             throws IOException {
