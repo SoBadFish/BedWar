a051d8147eddce7590799f8abc9de6465ca38272
Sobadfish <3098143605>
2023-07-13 09:28:17 +0800

    优化浮空字更新机制

--- src/main/java/org/sobadfish/bedwar/manager/FloatTextManager.java
+++ src/main/java/org/sobadfish/bedwar/manager/FloatTextManager.java
@@ -10,12 +10,19 @@ public class FloatTextManager {
     public static List<BedWarFloatText> floatTextList = new CopyOnWriteArrayList<>();
 
 
+    /**
+     * 请不要用这里的方法直接添加浮空字
+     * 使用{@link BedWarFloatText}中 showFloatText 的方法生成浮空字
+     * */
     public static void addFloatText(BedWarFloatText floatText){
         floatTextList.add(floatText);
     }
 
 
-
+    /**
+     * 请不要用这里的方法直接删除浮空字
+     * 使用{@link BedWarFloatText}中的方法toClose()方法浮空字
+     * */
     public static void removeFloatText(BedWarFloatText floatText) {
         floatTextList.remove(floatText);
     }
