51e7de6e45d1702713b45dd1947ecf4eeb2cfe2f
Sobadfish <3098143605>
2023-11-13 15:43:56 +0800

    优化快速搭路

--- src/main/java/org/sobadfish/bedwar/variable/TipVariable.java
+++ src/main/java/org/sobadfish/bedwar/variable/TipVariable.java
@@ -20,14 +20,14 @@ public class TipVariable extends BaseVariable {
     public void strReplace() {
         //等级
         PlayerData data = BedWarMain.getDataManager().getData(player.getName());
-        addStrReplaceString("%%bd-level%",data.getLevelString());
-        addStrReplaceString("%bd-exp%",data.getExpString(data.getExp())+"");
-        addStrReplaceString("%bd-nextExp%",data.getExpString(data.getNextLevelExp())+"");
-        addStrReplaceString("%bd-line%",data.getExpLine(10)+"");
-        addStrReplaceString("%bd-per%",String.format("%.2f",data.getExpPercent() * 100)+"");
-        addStrReplaceString("%bd-kill%",data.getFinalData(PlayerData.DataType.KILL)+"");
-        addStrReplaceString("%bd-victory%",data.getFinalData(PlayerData.DataType.VICTORY)+"");
-        addStrReplaceString("%bd-bed%",data.getFinalData(PlayerData.DataType.BED_BREAK)+"");
+        addStrReplaceString("{bd-level}",data.getLevelString());
+        addStrReplaceString("{bd-exp}",data.getExpString(data.getExp())+"");
+        addStrReplaceString("{bd-nextExp}",data.getExpString(data.getNextLevelExp())+"");
+        addStrReplaceString("{bd-line}",data.getExpLine(10)+"");
+        addStrReplaceString("{bd-per}",String.format("%.2f",data.getExpPercent() * 100)+"");
+        addStrReplaceString("{bd-kill}",data.getFinalData(PlayerData.DataType.KILL)+"");
+        addStrReplaceString("{bd-victory}",data.getFinalData(PlayerData.DataType.VICTORY)+"");
+        addStrReplaceString("{bd-bed}",data.getFinalData(PlayerData.DataType.BED_BREAK)+"");
 
     }
 }
