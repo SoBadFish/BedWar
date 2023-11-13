d3eeb4938638cfc52cec3bcc7bb21f66ecc12e7c
Sobadfish <3098143605>
2023-11-13 15:46:35 +0800

    优化快速搭路

--- src/main/java/org/sobadfish/bedwar/variable/BedWarVariable.java
+++ src/main/java/org/sobadfish/bedwar/variable/BedWarVariable.java
@@ -38,14 +38,14 @@ public class BedWarVariable extends BaseVariableV2 {
                     p+= room.getPlayerInfos().size();
                 }
             }
-            addVariable("{bd-"+worldRoom.getName()+"-player}",p+"");
+            addVariable("%bd-"+worldRoom.getName()+"-player%",p+"");
 
         }
         int game = 0;
         for(GameRoom gameRoom: BedWarMain.getRoomManager().getRooms().values()){
             game += gameRoom.getPlayerInfos().size();
         }
-        addVariable("{bd-all-player}",game+"");
+        addVariable("%bd-all-player%",game+"");
 
     }
 
@@ -67,8 +67,8 @@ public class BedWarVariable extends BaseVariableV2 {
                 default:break;
             }
         }
-        addVariable("{bd-"+roomConfig.getName()+"-player}",p+"");
-        addVariable("{bd-"+roomConfig.getName()+"-maxplayer}",mp+"");
-        addVariable("{bd-"+roomConfig.getName()+"-status}",status);
+        addVariable("%bd-"+roomConfig.getName()+"-player%",p+"");
+        addVariable("%bd-"+roomConfig.getName()+"-maxplayer%",mp+"");
+        addVariable("%bd-"+roomConfig.getName()+"-status%",status);
     }
 }
